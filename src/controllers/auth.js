2bace13f36f6d52da0cbad4c78fad4ae4b01df09
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-06 13:55:18 +0700

    update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,107 @@
 // import model here
+const { user } = require('../../models');
 
 // import package here
+const Joi = require('joi');
 
 exports.register = async (req, res) => {
-  // code here
+  try {
+    const data = req.body;
+
+    const schema = Joi.object({
+      name: Joi.string().min(3).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+      status: Joi.string().required(),
+    });
+
+    const { error } = schema.validate(data);
+
+    if (error) {
+      return res.send({
+        error: {
+          message: error.details[0].message,
+        },
+      });
+    }
+
+    const isAlready = await user.findOne({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (isAlready) {
+      return res.send({
+        error: {
+          message: `Account ${data.email} is Already`,
+        },
+      });
+    }
+
+    await user.create(data);
+
+    res.send({
+      message: 'register success',
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'Failed',
+      message: 'Server error',
+    });
+  }
 };
 
 exports.login = async (req, res) => {
-  // code here
+  try {
+    const data = req.body;
+
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    });
+
+    const { error } = schema.validate(data);
+
+    if (error) {
+      return res.send({
+        error: {
+          message: error.details[0].message,
+        },
+      });
+    }
+
+    const userExist = await user.findOne({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (!userExist) {
+      return res.send({
+        error: {
+          message: `Email or Password not match!`,
+        },
+      });
+    }
+
+    if (userExist.password != data.password) {
+      return res.send({
+        error: {
+          message: `Email or Password not match!`,
+        },
+      });
+    }
+
+    res.send({
+      message: 'login success',
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'Failed',
+      message: 'Server error',
+    });
+  }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
@@ -9,27 +9,30 @@ const {
   getUser,
   updateUser,
   deleteUser,
-} = require("../controllers/user");
-const { getProduct, addProduct } = require("../controllers/product");
+} = require('../controllers/user');
+const { getProduct, addProduct } = require('../controllers/product');
 const {
   getTransactions,
   addTransaction,
-} = require("../controllers/transaction");
-// import controller here
+} = require('../controllers/transaction');
+
+const { register, login } = require('../controllers/auth');
 
 // Route
-router.post("/user", addUsers);
-router.get("/users", getUsers);
-router.get("/user/:id", getUser);
-router.patch("/user/:id", updateUser);
-router.delete("/user/:id", deleteUser);
+router.post('/user', addUsers);
+router.get('/users', getUsers);
+router.get('/user/:id', getUser);
+router.patch('/user/:id', updateUser);
+router.delete('/user/:id', deleteUser);
 
-router.get("/products", getProduct);
-router.post("/product", addProduct);
+router.get('/products', getProduct);
+router.post('/product', addProduct);
 
-router.get("/transactions", getTransactions);
-router.post("/transaction", addTransaction);
+router.get('/transactions', getTransactions);
+router.post('/transaction', addTransaction);
 
 // add route here
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
